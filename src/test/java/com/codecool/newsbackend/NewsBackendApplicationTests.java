95d153bbeeeedaff625116a3a4c392289dbde4be
Vivien Walter <walter.vivien.k@gmail.com>
2020-06-22 11:16:41 +0200

    first commit

---
+++ src/test/java/com/codecool/newsbackend/NewsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecool.newsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
