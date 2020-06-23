e32680256becf623086fbff739354bee605694eb
sibaluca12 <sibaluca@gmail.com>
2020-06-23 14:58:20 +0200

    fetching data from api

--- src/main/java/com/codecool/newsbackend/controller/NewsController.java
+++ src/main/java/com/codecool/newsbackend/controller/NewsController.java
@@ -1,13 +1,22 @@
 package com.codecool.newsbackend.controller;
 
 
+import com.codecool.newsbackend.model.RestClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping
 public class NewsController {
+
+    @Autowired
+    RestClient restClient;
+
 
 
     @GetMapping("/dummy")
@@ -15,4 +24,23 @@ public class NewsController {
 
         return "hello world";
     }
+
+    @Autowired
+    RestTemplate restTemplate;
+
+
+    @Value("https://newsapi.org/v2/top-headlines?country=us&apiKey=00f7878a7a684b51a8f4eb8a56d4a033")
+    String serviceURL;
+
+
+    @CrossOrigin(origins = "http://localhost:3001")
+    @GetMapping("/availableOperations")
+    String getAvailableOperations() {
+        return restTemplate.getForObject(serviceURL, String.class);
+    }
+
+
+
+
+
 }
---
+++ src/main/java/com/codecool/newsbackend/service/RestService.java
@@ -0,0 +1,11 @@
+package com.codecool.newsbackend.service;
+
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class RestService {
+
+
+}
