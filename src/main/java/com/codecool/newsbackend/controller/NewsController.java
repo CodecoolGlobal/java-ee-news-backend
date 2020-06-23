cbb46784d17fc62295f370e08dafcc237a78bce4
Vivien Walter <walter.vivien.k@gmail.com>
2020-06-23 11:10:42 +0200

    add dummy controller

---
+++ src/main/java/com/codecool/newsbackend/controller/NewsController.java
@@ -0,0 +1,18 @@
+package com.codecool.newsbackend.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class NewsController {
+
+
+    @GetMapping("/dummy")
+    public String dummy() {
+
+        return "hello world";
+    }
+}
